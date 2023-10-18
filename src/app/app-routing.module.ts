463552724874d0be8fa6e7bf2de8cbe0bc63bd7f
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-10-18 09:50:02 +0200

    routing e start home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ChisonoComponent } from './pages/chisono/chisono.component';
+import { ContattiComponent } from './pages/contatti/contatti.component';
+import { PrenotaComponent } from './pages/prenota/prenota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'chisono',
+    component: ChisonoComponent
+  },
+  {
+    path: 'contatti',
+    component: ContattiComponent
+  },
+  {
+    path: 'prenota',
+    component: PrenotaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
